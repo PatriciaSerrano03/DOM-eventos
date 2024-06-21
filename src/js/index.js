22075774426f1dd9511f531aec902ac3f6c51ac1
PatriiSerrano <patricia.serranomolina@gmail.com>
2024-06-21 10:18:25 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,27 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+//DOM - Event
+const ButtonElement = document.getElementById('button');
+
+const printClick = () => {
+  console.log('CLICK');
+};
+
+ButtonElement.addEventListener('click', printClick);
+
+//DOM - Objetos
+const Button2Element = document.getElementById('button2');
+
+const printClick2 = event => {
+  console.log(event);
+};
+
+Button2Element.addEventListener('click', printClick2);
+
+//Escuchar un Scroll
+/* const ViewScroll = event => {
+  console.log(event);
+};
+
+window.addEventListener('scroll', ViewScroll); */
